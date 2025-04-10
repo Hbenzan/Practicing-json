5cab80feb944387a5829231b3c86f615ac72cdbc
hayesbenzan <hayes_benzan25@milton.edu>
2025-04-10 10:21:07 -0400

    finished graded hw

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var allbooks = data.length
 
 const readbooks = data.filter(checkRead);
 
-function checkRead (read){
-    console.log(read["read?"]);
-    if("Yes" == read["read?"]){
-        return read;
-    }
-}
+// function checkRead (read){
+//     console.log(read["read?"]);
+//     if("Yes" == read["read?"]){
+//         return read;
+//     }
+// }
 
 console.log(readbooks.length)
 
@@ -59,15 +59,96 @@ const counts = []
 
 for (let i = 0; i < uniqueData.length; i++) {
     console.log (uniqueData[i]);
-const fantasy = data.filter(item => {
-    if (item["category"] === uniqueData[i]) {
-        return(item);
-    }
-});
-console.log (fantasy.length);
+    const fantasy = data.filter(item => {
+        if (item["category"] === uniqueData[i]) {
+            return(item);
+        }
+    });
+    console.log (fantasy.length);
 
+  counts.push (fantasy.length);
 
-//uniqueData.forE
+    //uniqueData.forE
 
-//console.log(uniqueData) 
+    //console.log(uniqueData) 
 }
+console.log(counts);
+
+function findMax(counts) {
+    if (counts.length === 0) {
+    }
+    let largest = counts[0];
+    for (let i = 1; i < counts.length; i++) {
+      if (counts[i] > largest) {
+        largest = counts[i];
+      }
+    }
+    return largest;
+  }
+  
+  const max = findMax(counts);
+  const index = counts.indexOf (max);
+  console.log (index);
+  console.log(max); // Output: 8
+
+
+
+  uniqueData.at (index);
+  console.log (uniqueData.at(index));
+
+
+
+  //filter out the books that haven't been read / or use for loop
+
+  console.log(readbooks.length);
+
+
+  function checkRead (read){
+      console.log(read["read?"]);
+      if("Yes" == read["read?"]){
+          return read;
+      }
+  }
+  
+  console.log(readbooks.length)
+
+  const yearCounts = {};
+  
+  let maxYear = "";
+  let maxBook = 0;
+
+
+
+
+// Loop through each read book to get the year it was bought and count how many books were read that year
+  for (const book of readbooks) {
+
+    const datastring = book ["Date Bought"];
+    
+    // Get the last 4 characters from the "Date Bought" string to get the year
+    const year = datastring.charAt(datastring.length - 4) +
+    datastring.charAt(datastring.length - 3) +
+    datastring.charAt(datastring.length - 2) +
+    datastring.charAt(datastring.length - 1);
+
+    // Count how many books were read in that year
+    if (!yearCounts[year]){
+        yearCounts[year] = 1;
+    } else {
+        yearCounts[year] += 1;
+    }
+    }
+
+// Loop through the `yearCounts` object to find the year with the most books read
+    for(const year in yearCounts) {
+        if (yearCounts[year] > maxBook) {
+            maxYear = year;
+            maxBook = yearCounts[year];
+        }
+    }
+    
+    // Print the year with the most books read and the number of books read that year
+    console.log(maxYear);
+    console.log(maxBook)
+
+  
